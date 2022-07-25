2094ca3bd0f9cc611131feea564b6fe93e3bc451
Gulmira <gchuroko@stumail.jccc.edu>
2022-07-25 00:34:05 -0700

    Part 2 done

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    private String description;
+
+    public Skill(String adescription) {
+        super();
+        this.description = adescription;
+    }
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
