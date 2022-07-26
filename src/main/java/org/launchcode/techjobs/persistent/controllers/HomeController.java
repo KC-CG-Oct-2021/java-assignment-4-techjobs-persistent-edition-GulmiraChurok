b0e2e8b47f61ac6c2fb0cb719d2c2f4b07c088b2
Gulmira <gchuroko@stumail.jccc.edu>
2022-07-26 13:48:05 -0700

    all done3

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -47,11 +47,7 @@ public class HomeController {
     }
 
     @PostMapping("add")
-    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
-                                     Errors errors, Model model, @RequestParam int employerId,
-                                    @RequestParam List<Integer> skills
-    ) {
-
+    public String processAddJobForm(@ModelAttribute @Valid Job newJob, Errors errors, Model model, @RequestParam int employerId, @RequestParam List<Integer> skills) {
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add Job");
             model.addAttribute("employers", employerRepository.findAll());
